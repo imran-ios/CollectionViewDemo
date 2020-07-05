25123b24efa4f6f0369e91a9481f3ddeccf96b51
Md Imran Ali <mdimranali94@gmail.com>
2020-07-05 16:25:33 +0530

    first commit

---
+++ CollectionViewDemo/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Kann on 16/10/19.
+//  Copyright © 2019 Kann. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
